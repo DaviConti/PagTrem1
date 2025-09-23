304428f2208f61534b1bea68771b9cb236aa6426
MathKing1 <matheus_n_cardoso@estudante.sesisenai.org.br>
2025-09-23 11:54:55 -0300

    add

--- public/contatos.php
+++ public/contatos.php
@@ -1,3 +1,28 @@
+<?php
+session_start();
+if (!isset($_SESSION['username'])) {
+    header('Location: login.php');
+    exit();
+}
+
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "login_db";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+$sql = "SELECT username, nome, cargo FROM ususarios ORDER BY username";
+$result = $conn->query($sql);
+$contacts = $result->fetch_all(MYSQLI_ASSOC);
+$conn->close();
+?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
@@ -8,15 +33,19 @@
 </head>
 
 <body>
-  <a href="../public/chat.html">
-    <div class="logo">
-      <img src="../img/unnamed.png" alt="Logo" width="420" height="250">
-    </div>
-    <div class="info">
-      <h1>Contatos</h1>
-      <p>Clique para conversar com o suporte</p>
-    </div>
-  </a>
+  <div class="logo">
+    <img src="../img/unnamed.png" alt="Logo" width="420" height="250">
+  </div>
+  <div class="info">
+    <h1>Contatos</h1>
+    <p>Clique para conversar com o suporte</p>
+    <ul>
+      <?php foreach ($contacts as $contact): ?>
+        <li><?php echo htmlspecialchars($contact['nome'] ?: $contact['username']) . ' (' . $contact['cargo'] . ')'; ?></li>
+      <?php endforeach; ?>
+    </ul>
+    <a href="chat.php">Ir para o Chat</a>
+  </div>
 </body>
 
 </html>
