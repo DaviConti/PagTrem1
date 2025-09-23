f158c32dc64c6285cb6d95a0a5a0c34b2a67080b
nicolasyan29 <nicolas_borba@estudante.sesisenai.org.br>
2025-09-23 11:54:28 -0300

    add

--- public/telaInicial.php
+++ public/telaInicial.php
@@ -4,16 +4,16 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title>Página Inicial</title>
-  <link rel="stylesheet" href="../style/inicial.css" />
+  <link rel="stylesheet" href="../style/telainicial.css" />
 </head>
 
 <body>
   <a href="../public/login.html">
     <div id="logo">
-      <img src="../img/icone pagtrem-Photoroom.png" alt="Logo" width="420" height="250" />
+      <img src="../img/icone pagtrem-Photoroom.png" alt="Logo" width="380" height="250" />
     </div>
     <div>
-      <h3>Clique na tela para continuar</h3>
+      <h1>Clique na tela para continuar</h1>
     </div>
   </a>
 
